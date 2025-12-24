1f220466d7ecfca0c6cbb649139dd1939c47c463
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2025-12-24 11:08:38 +0000

    Added `ParsedEnuDef.parse`, `EnumVariant` and `EnumVariantField`

--- guppylang-internals/src/guppylang_internals/definition/struct.py
+++ guppylang-internals/src/guppylang_internals/definition/struct.py
@@ -238,6 +238,7 @@ class ParsedStructDef(TypeDef, CheckableDef):
         fields = [
             StructField(f.name, type_from_ast(f.type_ast, ctx)) for f in self.fields
         ]
+        print("fields: ", fields)
         return CheckedStructDef(
             self.id, self.name, self.defined_at, self.params, fields
         )
