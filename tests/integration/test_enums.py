2e88072f28423c37c67de51b8a258afc63f790e5
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-01-06 16:32:38 +0100

    mypy

--- tests/integration/test_enums.py
+++ tests/integration/test_enums.py
@@ -3,40 +3,40 @@ import pytest
 from guppylang import guppy
 from tests.util import compile_guppy
 
-"""
-Stating from a Rust example:
-rust
-enum Message {
-    Quit,
-    Resize { width: i32, height: i32},
-    Move {x: u64, y: u64},
-    Echo(String),
-    ChangeColor(i32, i32, i32),
-}
-
-fn main() {
-    let messages: [Message; 5] = [
-        Message::Resize {
-            width: 10,
-            height: 30,
-        },
-        Message::Move { x: 10, y: 15 },
-        Message::Echo(String::from("hello world")),
-        Message::ChangeColor(200, 255, 255),
-        Message::Quit,
-    ];
-
-}
-"""
-
 
 @pytest.mark.skip
 def test_enum():
+    """
+    Stating from a Rust example:
+    rust
+    enum Message {
+        Quit,
+        Resize { width: i32, height: i32},
+        Move {x: u64, y: u64},
+        Echo(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    fn main() {
+        let messages: [Message; 5] = [
+            Message::Resize {
+                width: 10,
+                height: 30,
+            },
+            Message::Move { x: 10, y: 15 },
+            Message::Echo(String::from("hello world")),
+            Message::ChangeColor(200, 255, 255),
+            Message::Quit,
+        ];
+
+    }
+    """
+
     @guppy.enum
     class Message:
-        Quit = {}
-        Resize = {"width": int, "height": int}
-        Move = {"x": int, "y": int}
+        Quit = {}  # noqa: RUF012
+        Resize = {"width": int, "height": int}  # noqa: RUF012
+        Move = {"x": int, "y": int}  # noqa: RUF012
         Echo = str
         ChangeColor = (int, int, int)
 
