1a057ecdf4d2c8d56c01632a3f00575464b3fed4
Mark Koch <mark.koch@quantinuum.com>
2026-01-14 12:49:26 +0000

    Update tests

--- tests/error/comptime_arg_errors/dependent_mismatch2.py
+++ tests/error/comptime_arg_errors/dependent_mismatch2.py
@@ -7,8 +7,14 @@ def foo(n: nat @comptime, xs: "array[int, n]") -> None: ...
 
 
 @guppy
-def main(n: nat @comptime, m: nat @comptime) -> None:
+def bar(n: nat @comptime, m: nat @comptime) -> None:
     foo(n, array(i for i in range(m)))
+
+
+@guppy
+def main() -> None:
+    bar(42, 42)  # This succeeds
+    bar(42, 43)  # This fails
 
 
 main.compile_function()
--- tests/error/poly_errors_py312/linear_by_default.py
+++ tests/error/poly_errors_py312/linear_by_default.py
@@ -1,11 +1,17 @@
+from guppylang import qubit
 from guppylang.decorator import guppy
 from guppylang.std.lang import owned
 
 
 @guppy
-def main[T](x: T @ owned) -> tuple[T, T]:
+def foo[T](x: T @ owned) -> tuple[T, T]:
     # T is not copyable
     return x, x
+
+
+@guppy
+def main() -> tuple[qubit, qubit]:
+    return foo(qubit())
 
 
 main.compile()
