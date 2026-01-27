e2e05bef51d58fb5b80983b232e8131b6a3555af
Robert Fisher <fisher.robert26@gmail.com>
2026-01-27 12:37:12 -0600

    Update tests/error/array_errors/array_index_nested_array_outer.py
    Co-authored-by: nicolaassolini-qntm <nicola.assolini@quantinuum.com>

--- tests/error/array_errors/array_index_nested_array_outer.py
+++ tests/error/array_errors/array_index_nested_array_outer.py
@@ -2,7 +2,7 @@ from guppylang.decorator import guppy
 from guppylang.std.builtins import array
 
 @guppy
-def main() -> None:
+def main() -> int:
       matrix = array(array(i + j for i in range(10)) for j in range(5))
       return matrix[5][0] 
 
