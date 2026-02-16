746b731bfd2462b125ce34f094b91192ca48dfcb
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-02-16 15:19:39 +0000

    all the test, apart from generics

--- tests/integration/test_enums.py
+++ tests/integration/test_enums.py
@@ -1,5 +1,35 @@
 from guppylang import guppy
+from matplotlib.pyplot import cla
+import pytest
 from tests.util import compile_guppy
+
+from typing import Generic, TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+"""
+The syntax is inspired by Rust:
+
+enum Message {
+    Quit,
+    Resize { width: i32, height: i32},
+    Move {x: u64, y: u64},
+}
+
+fn main() {
+    let messages: [Message; 5] = [
+        Message::Resize {
+            width: 10,
+            height: 30,
+        },
+        Message::Move { x: 10, y: 15 },
+        Message::Quit,
+    ];
+
+}
+"""
 
 
 # TODO: NICOLA temporary test, to be substituted with `test_basic_enum`
@@ -21,48 +51,220 @@ def test_enum_syntax():
     EmptyEnum.compile()
     GenericEnum.compile()
 
+#######################################################################
 
 def test_basic_enum(validate):
-    """
-    Stating from a Rust example:
-    rust
-    enum Message {
-        Quit,
-        Resize { width: i32, height: i32},
-        Move {x: u64, y: u64},
-        Echo(String),
-        ChangeColor(i32, i32, i32),
-    }
-
-    fn main() {
-        let messages: [Message; 5] = [
-            Message::Resize {
-                width: 10,
-                height: 30,
-            },
-            Message::Move { x: 10, y: 15 },
-            Message::Echo(String::from("hello world")),
-            Message::ChangeColor(200, 255, 255),
-            Message::Quit,
-        ];
-
-    }
-    """
+    @guppy.enum
+    class EmptyEnum:
+        pass
 
     @guppy.enum
-    class Message:
-        Quit = {}  # noqa: RUF012
+    class OneVariantEnum:
+        VariantA = {}  # noqa: RUF012
+
+    @guppy.enum
+    class TwoVariantEnum:
+        VariantA = {}  # noqa: RUF012
+        VariantB = {"x": int, "y": float}  # noqa: RUF012
+
+    @guppy.enum
+    class DocstringEnum:
+        """This is an enum with a docstring!"""
+        VariantA = {}  # noqa: RUF012
+        VariantB = {"x": int}  # noqa: RUF012
+
+    @guppy.enum
+    class MethodEnum:
+        Empty = {}  # noqa: RUF012
         Resize = {"width": int, "height": int}  # noqa: RUF012
+        Quit = {"x": int}  # noqa: RUF012
 
         @guppy
         def method(self) -> str:
             return "42"
 
     @compile_guppy
-    def use_enum() -> None:
-        variant1 = Message.Resize(2, 3)
-        # variant2 = Message.Quit()
+    def main(
+        e: EmptyEnum,
+        one: OneVariantEnum,
+        two: TwoVariantEnum,
+        doc: DocstringEnum,
+        meth: MethodEnum,
+    ) -> None:
+        var1 = OneVariantEnum.VariantA()
+        two_var1 = TwoVariantEnum.VariantA()
+        two_var2 = TwoVariantEnum.VariantB(1, 2.0)
+        var3 = DocstringEnum.VariantB(3)
+        var4 = DocstringEnum.VariantA()
+        variant1 = MethodEnum.Resize(2, 3)
+        variant2 = MethodEnum.Quit(1)
+        variant3 = MethodEnum.Empty()
         variant1.method()
-        # variant2.method()
+        variant2.method()
+        variant3.method()
 
-    validate(use_enum)
+    validate(main)
+
+
+def test_backward_ref_enum(validate):
+    @guppy.enum
+    class EnumA:
+        VariantA = {"x": int}  # noqa: RUF012
+
+    @guppy.enum
+    class EnumB:
+        VariantB = {"y": EnumA}  # noqa: RUF012
+
+    @guppy
+    def main(a: EnumA, b: EnumB) -> EnumB:
+        e1 = EnumB.VariantB(a)
+        return EnumB.VariantB(EnumA.VariantA(1))
+
+    validate(main.compile_function())
+
+
+def test_forward_ref(validate):
+    @guppy.enum
+    class EnumA:
+        VariantA = {"x": "EnumB"}  # Forward reference
+
+    @guppy.enum
+    class EnumB:
+        VariantB = {"y": int}
+
+    @guppy
+    def main(a: EnumA, b: EnumB) -> EnumA:
+        EnumA.VariantA(b)
+        return EnumA.VariantA(EnumB.VariantB(1))
+
+    validate(main.compile_function())
+
+#
+@pytest.mark.skip
+def test_generic(validate):
+    S = guppy.type_var("S")
+    T = guppy.type_var("T")
+
+    @guppy.enum
+    class EnumA(Generic[T]):
+        VariantA = {"x": tuple[int, T]}
+
+    @guppy.enum
+    class EnumC:
+        VariantA = {"a": EnumA[int]}
+        VariantB = {"b": EnumA[list[bool]]}
+        VariantC = {"c": "EnumB[float, EnumB[bool, int]]"}
+
+    @guppy.enum
+    class EnumB(Generic[S, T]):
+        VariantA = {"x": S, "y": EnumA[T]}
+
+    @guppy
+    def main(a: EnumA[EnumA[float]], b: EnumB[bool, int], c: EnumC) -> None:
+        x = EnumA.VariantA((0, False))
+        y = EnumA.VariantA((0, -5))
+        EnumA.VariantA((0, x))
+        EnumB.VariantA(x=42, y=a)
+        EnumC.VariantA(a=y)
+        EnumC.VariantB(b=EnumA.VariantA((0, [])))
+        EnumC.VariantC(c=EnumB.VariantA(x=42.0, y=EnumA.VariantA((4, b))))
+
+    validate(main.compile_function())
+
+
+# TODO: Methods now are limited due to the fact that we cannot access to enum fields in 
+# and we do not have pattern matching
+def test_methods(validate):
+    @guppy.enum
+    class EnumA:
+        VariantA = {"x": int}
+
+        @guppy
+        def foo(self: "EnumA", y: int) -> int:
+            return 2 + y
+
+    @guppy.enum
+    class EnumB:
+        VariantA = {"x": int, "y": float}
+
+        @guppy
+        def bar(self: "EnumB", a: EnumA) -> float:
+            return a.foo(6) + 5.1
+
+    @guppy
+    def main(a: EnumA, b: EnumB) -> tuple[int, float]:
+        return a.foo(1), b.bar(a)
+
+    validate(main.compile_function())
+
+#TODO: after Generics fix, to remove
+def test_higher_order_easy(validate):
+
+    @guppy.enum
+    class Enum():
+        VariantA = {"x": int}
+
+    @guppy
+    def factory(mk_enum: "Callable[[int], Enum]", x: int) -> Enum:
+        return mk_enum(x)
+
+    @guppy
+    def main() -> None:
+        factory(Enum.VariantA, 42)
+
+    validate(main.compile_function())
+
+
+@pytest.mark.skip
+def test_higher_order(validate):
+    T = guppy.type_var("T")
+
+    @guppy.enum
+    class Enum(Generic[T]):
+        VariantA = {"x": T}
+
+    @guppy
+    def factory(mk_enum: "Callable[[int], Enum[int]]", x: int) -> Enum[int]:
+        return mk_enum(x)
+
+    @guppy
+    def main() -> None:
+        factory(Enum.VariantA, 42)
+
+    validate(main.compile_function())
+
+
+def test_wiring(validate):
+    @guppy.enum
+    class MyEnum:
+        VariantA = {"x": int}
+
+    @guppy
+    def foo() -> MyEnum:
+        s = 0
+        # This tests that reassigning `s` invalidates the old `s = 0` wire when
+        # compiling to Hugr.
+        s = MyEnum.VariantA(42)
+        return s
+
+    validate(foo.compile_function())
+
+
+
+def test_redefine(validate):
+    """See https://github.com/quantinuum/guppylang/issues/1107"""
+
+    @guppy.enum
+    class MyEnum:
+        VariantA = {"x": int}
+
+    @guppy.enum
+    class MyEnum:  # noqa: F811
+        VariantB = {}
+
+    @guppy
+    def foo() -> MyEnum:
+        return MyEnum.VariantB()
+
+    validate(foo.compile_function())
+
