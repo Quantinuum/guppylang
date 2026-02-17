e7764c12a985623f0c9570d57b92c128c40872a0
Maximilian Rüsch <maximilian.ruesch@quantinuum.com>
2026-02-17 16:31:39 +0000

    Add visibility to smoketest

--- tests/integration/test_library.py
+++ tests/integration/test_library.py
@@ -4,12 +4,13 @@ from hugr.package import Package
 from guppylang import guppy
 
 
-# TODO deduplicate
-def _func_names(package: Package) -> set[str]:
+def _func_names_and_visibilities(package: Package) -> set[tuple[str, str]]:
     hugr = package.modules[0]
 
     return {
-        n.op.f_name for n in hugr.values() if isinstance(n.op, (FuncDefn, FuncDecl))
+        (n.op.f_name, n.op.visibility)
+        for n in hugr.values()
+        if isinstance(n.op, (FuncDefn, FuncDecl))
     }
 
 
@@ -27,6 +28,9 @@ def test_smoke_test_library(validate):
         def member(self) -> None:
             pass
 
+        @guppy.declare
+        def member_decl(self) -> None: ...
+
     library = guppy.library(
         func_1,
         func_2,
@@ -35,8 +39,21 @@ def test_smoke_test_library(validate):
 
     compiled_library = library.compile()
     validate(compiled_library)
-    assert _func_names(compiled_library) == {
-        "tests.integration.test_library.test_smoke_test_library.<locals>.func_1",
-        "tests.integration.test_library.test_smoke_test_library.<locals>.func_2",
-        "tests.integration.test_library.test_smoke_test_library.<locals>.MyStruct.member",
+    assert _func_names_and_visibilities(compiled_library) == {
+        (
+            "tests.integration.test_library.test_smoke_test_library.<locals>.func_1",
+            "Public",
+        ),
+        (
+            "tests.integration.test_library.test_smoke_test_library.<locals>.func_2",
+            "Public",
+        ),
+        (
+            "tests.integration.test_library.test_smoke_test_library.<locals>.MyStruct.member",
+            "Public",
+        ),
+        (
+            "tests.integration.test_library.test_smoke_test_library.<locals>.MyStruct.member_decl",
+            "Public",
+        ),
     }
