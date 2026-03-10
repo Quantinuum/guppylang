1005c6867a17ae8ca3419b55fc015b64e56a7753
Maximilian Rüsch <maximilian.ruesch@quantinuum.com>
2026-03-10 14:51:53 +0000

    Add structural subtyping test

--- tests/integration/library/test_struct.py
+++ tests/integration/library/test_struct.py
@@ -23,3 +23,30 @@ def test_struct_defn():
 
     results = main.emulator(n_qubits=1, libs=[lib]).run().results[0].entries
     assert results == [("result", 5)]
+
+
+def test_structural_subtyping():
+    @guppy.struct
+    class Foo:
+        x: int
+
+    @guppy.struct
+    class Bar:
+        x: int
+
+    @guppy.declare(link_name="my_name")
+    def do_foo(f: Foo) -> int: ...
+
+    @guppy(link_name="my_name")
+    def do_foo_impl(f: Bar) -> int:
+        return f.x
+
+    lib = guppy.library(Bar, do_foo_impl).compile()
+
+    @guppy
+    def main() -> None:
+        f = Foo(4)
+        result("result", do_foo(f))
+
+    results = main.emulator(n_qubits=1, libs=[lib]).run().results[0].entries
+    assert results == [("result", 4)]
