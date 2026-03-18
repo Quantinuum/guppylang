4625c10b5361a8eb942182c96d1175db3ec7c8ca
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-03-18 12:18:27 +0000

    remove unused field import from dataclasses in modifier.py

--- guppylang-internals/src/guppylang_internals/definition/modifier.py
+++ guppylang-internals/src/guppylang_internals/definition/modifier.py
@@ -1,5 +1,5 @@
 from collections.abc import Callable
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import TYPE_CHECKING, Any
 
 from guppylang_internals.definition.common import (
