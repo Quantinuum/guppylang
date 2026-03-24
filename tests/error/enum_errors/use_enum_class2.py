0cc798e304f384035c7da74d255127b9156d4b83
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-03-24 12:43:23 +0000

    fix: Raise error when using an enum class in places where a variant is expected (#1575)

--- tests/error/enum_errors/use_enum_class2.py
+++ tests/error/enum_errors/use_enum_class2.py
@@ -1,11 +1,15 @@
 from guppylang import guppy
 from tests.util import compile_guppy
 
+
 @guppy.enum
 class MyEnum:
+    Left = {} 
+
+@guppy
+def g(e : MyEnum) -> None:
     pass
 
 @compile_guppy
 def fun() -> None:
-    x = MyEnum()
-
+    g(MyEnum)
