7976b14cc7d94781dcc44b4279da36060c712886
Mark Koch <48097969+mark-koch@users.noreply.github.com>
2026-04-02 10:38:38 +0100

    fix: Correctly store stack frames for nested function definitions (#1625)

--- guppylang-internals/src/guppylang_internals/compiler/core.py
+++ guppylang-internals/src/guppylang_internals/compiler/core.py
@@ -182,16 +182,6 @@ class CompilerContext(ToHugrContext):
 
         Might mutate the current Hugr if this definition has never been compiled before.
         """
-        # TODO: The check below is a hack to support nested function definitions. We
-        #  forgot to insert frames for nested functions into the DEF_STORE, which would
-        #  make the call to `ENGINE.get_checked` below fail. For now, let's just short-
-        #  cut if the function doesn't take any generic params (as is the case for all
-        #  nested functions).
-        #  See https://github.com/quantinuum/guppylang/issues/1032
-        if (def_id, ()) in self.compiled:
-            assert type_args == []
-            return self.compiled[def_id, ()], type_args
-
         mono_args: PartiallyMonomorphizedArgs | None = None
         rem_args = type_args
         compile_outer: Callable[[], CompiledDef]
