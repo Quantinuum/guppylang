91a868fa34fe97f4fc952e28fdb946440aaa328d
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-04-07 14:37:34 +0100

    add error message for power constraint violation in qubit initialization test

--- tests/error/modifier_errors/flag_power_qubit2.py
+++ tests/error/modifier_errors/flag_power_qubit2.py
@@ -1,21 +1,16 @@
-from guppylang import qubit, guppy
+from guppylang import array, qubit, guppy
 from guppylang.std.builtins import owned
 
-@guppy.struct
-class QubitPair:
-    q1: qubit
-    q2: qubit
-
 @guppy.declare
-def init_pair() -> QubitPair: ...
+def init_qubits() -> array[qubit, 2]: ...
 
 @guppy.declare(unitary=True)
-def uni_discard(q: QubitPair @owned) -> None: ...
+def uni_discard(q: array[qubit, 2] @owned) -> None: ...
 
 
 @guppy(power=True)
 def test() -> None:
-    qp = init_pair()
+    qp = init_qubits()
     uni_discard(qp)
     
 
