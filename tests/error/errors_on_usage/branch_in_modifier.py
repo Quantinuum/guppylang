8ea57294e33363c72f3cc3df627f2bd0cdbaac9b
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-05-01 17:06:34 +0100

    comment about an issue

--- tests/error/errors_on_usage/branch_in_modifier.py
+++ tests/error/errors_on_usage/branch_in_modifier.py
@@ -3,9 +3,10 @@ from guppylang.std.builtins import power
 
 @guppy
 def test(b: bool) -> int:
+    x = 0 # error: if x defined here we do not get the error
     with power(2):
         if b:
-            x = 3
+            x = 3 # probably here is not visited?
     a = b
     c = x +1
     return 0
