4284ee96a2770ede34eb97fc13ddb4ad854e39aa
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2026-05-11 19:41:21 +0100

    no, test no covariance of return type, simpler right

--- tests/error/type_errors/fun_ty_mismatch_4.py
+++ tests/error/type_errors/fun_ty_mismatch_4.py
@@ -2,9 +2,11 @@ from collections.abc import Callable
 
 from tests.util import compile_guppy
 
+
 @compile_guppy
-def foo() -> Callable[[nat], bool]:
-    def bar(x: int) -> bool:
-        return x > 0
+def foo() -> Callable[[nat], int]:
+    # This has a narrower return type, but we enforce invariance of Callable types, so this is still an error.
+    def bar(x: nat) -> nat:
+        return x
 
     return bar
