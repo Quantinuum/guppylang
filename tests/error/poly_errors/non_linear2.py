e53254877d24ffaaa6380ed37d40acdfe78cec8c
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2026-06-05 13:14:46 +0100

    correct issue link

--- tests/error/poly_errors/non_linear2.py
+++ tests/error/poly_errors/non_linear2.py
@@ -7,8 +7,8 @@ from guppylang.std.effects import effects
 T = guppy.type_var("T")
 
 
-# Pending https://github.com/Quantinuum/guppylang/issues/1752 we need to explicitly
-# declare the effects of `f`
+# Pending https://github.com/Quantinuum/guppylang/issues/1760 we need to explicitly
+# declare the effects of `x`
 @guppy.declare
 def foo(x: Callable[[T], T] @ effects()) -> None: ...
 
