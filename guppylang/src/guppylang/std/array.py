97c69624a1181804e8b2c1685941a394bae84d19
Nicola Assolini <nicola.assolini@quantinuum.com>
2026-06-11 10:59:20 +0100

    refactor!: Make stdlib types state mutable with private fields (#1840)

--- guppylang/src/guppylang/std/array.py
+++ guppylang/src/guppylang/std/array.py
@@ -258,22 +258,23 @@ class array(builtins.list[_T], Generic[_T, _n]):
         return [*args]
 
 
-@guppy.struct(frozen=True)
+@guppy.struct
 class ArrayIter(Generic[L, n]):
     """Iterator over arrays."""
 
-    xs: array[L, n]
-    i: int
+    _xs: array[L, n]
+    _i: int
 
     @guppy
     @no_type_check
     def __next__(
-        self: ArrayIter[L, n] @ owned,
-    ) -> Option[tuple[L, ArrayIter[L, n]]]:
-        if self.i < int(n):
-            elem = self.xs.take(self.i)
-            return some((elem, ArrayIter(self.xs, self.i + 1)))
-        self.xs.discard_all_taken()
+        self: Self @ owned,
+    ) -> Option[tuple[L, Self]]:
+        if self._i < int(n):
+            elem = self._xs.take(self._i)
+            self._i += 1
+            return some((elem, self))
+        self._xs.discard_all_taken()
         return nothing()
 
 
@@ -325,18 +326,20 @@ class frozenarray(Generic[T, n]):
         return array(x for x in self)
 
 
-@guppy.struct(frozen=True)
+@guppy.struct
 class FrozenarrayIter(Generic[T, n]):
     """Iterator for frozenarrays."""
 
-    xs: frozenarray[T, n]  # type: ignore[type-arg]
-    i: int
+    _xs: frozenarray[T, n]  # type: ignore[type-arg]
+    _i: int
 
     @guppy
     @no_type_check
     def __next__(
-        self: FrozenarrayIter[T, n],
-    ) -> Option[tuple[T, FrozenarrayIter[T, n]]]:
-        if self.i < int(n):
-            return some((self.xs[self.i], FrozenarrayIter(self.xs, self.i + 1)))
+        self: Self @ owned,
+    ) -> Option[tuple[T, Self]]:
+        if self._i < int(n):
+            elem = self._xs[self._i]
+            self._i += 1
+            return some((elem, self))
         return nothing()
