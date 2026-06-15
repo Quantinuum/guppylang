6c67f071e336cc81d295cb03db7b8e52e1b7cd01
Craig Roy <croyzor@users.noreply.github.com>
2026-06-15 13:47:16 +0100

    feat!: Protocol checking (#1765)

---
+++ tests/error/protocol_errors_py312/no_impl.py
@@ -0,0 +1,17 @@
+from guppylang import guppy
+from guppylang.std.builtins import nat
+
+@guppy.protocol
+class Proto[T]:
+    @guppy.require
+    def foo[T](t: T) -> T: ...
+
+@guppy
+def bar[P: Proto[nat]](p: P, n: nat) -> nat:
+    return p.foo(n)
+
+@guppy
+def baz() -> nat:
+    return bar(nat, 42)
+
+baz.compile()
