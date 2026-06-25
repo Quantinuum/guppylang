b34ba6d00ec57e9d62cdad5ddd34050c448192e2
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-06-25 15:29:51 +0100

    cleaned test

--- tests/error/metadata_errors/reversed_keyword.py
+++ tests/error/metadata_errors/reversed_keyword.py
@@ -4,8 +4,6 @@ from guppylang.decorator import metadata
 
 @guppy
 @metadata("tket.unitary", "value1")
-def main() -> None:...
-    # """Main function for the GuppyLang program."""
-    # pass
+def main() -> None:
+    pass
 
-main.compile()
