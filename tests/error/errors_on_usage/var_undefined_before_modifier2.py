5c6e13fba73eba5e3fa1e94bfc075f79a98fc3b2
Fernando <ferbetanzo@gmail.com>
2026-07-14 16:07:09 +0100

    chore: update snapshots

--- tests/error/errors_on_usage/var_undefined_before_modifier2.py
+++ tests/error/errors_on_usage/var_undefined_before_modifier2.py
@@ -4,7 +4,7 @@ from guppylang.std.builtins import power
 @guppy
 def main(b: bool) -> None:
     x = 3
-    
+
     if b:
         y = 2
     a = i
@@ -13,5 +13,4 @@ def main(b: bool) -> None:
     a = y
 
 
-                
 main.check()
