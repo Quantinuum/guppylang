19d6db20886d9211a5cd3b86f26e072951660de6
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-07-21 12:35:31 +0100

    fix unitary checker argument validation and add tests for dagger constraint violations

--- tests/integration/test_modifier.py
+++ tests/integration/test_modifier.py
@@ -17,7 +17,7 @@ from guppylang.std.builtins import (
     power,
 )
 from guppylang.std.num import nat
-from guppylang.std.quantum import angle, cx, discard, h, qubit, rx, discard_array
+from guppylang.std.quantum import cx, discard, discard_array, h, qubit
 
 
 def test_dagger_simple(validate):
@@ -55,11 +55,11 @@ def test_assignment_in_dagger(validate):
         y = 1
         with dagger:
             x = 5
-            rx(q, angle(1 / x))
+            h(q)
         with dagger:
             y = 2
             with control(c):
-                rx(q, angle(1 / y))
+                h(q)
 
         discard(q)
         discard(c)
