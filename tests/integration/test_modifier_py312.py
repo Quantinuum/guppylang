e5045b3edfc51b93fb775cd1e619820cad27cf02
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-07-30 11:42:51 +0100

    Fix nat vars in modifier blocks

--- tests/integration/test_modifier_py312.py
+++ tests/integration/test_modifier_py312.py
@@ -1,7 +1,28 @@
 from guppylang.decorator import guppy
 
-from guppylang.std.builtins import Controllable, Daggerable, Unitary, control, dagger
+from guppylang.std.array import array
+from guppylang.std.builtins import (
+    Controllable,
+    Daggerable,
+    Unitary,
+    control,
+    dagger,
+    nat,
+)
 from guppylang.std.quantum import h, qubit
+
+
+def test_generic_nat_in_dagger(validate):
+    @guppy
+    def apply_dagger[n: nat](qs: array[qubit, n]) -> None:
+        with dagger:
+            h(qs[n - 1])
+
+    @guppy
+    def main(qs: array[qubit, 2]) -> None:
+        apply_dagger(qs)
+
+    validate(main.compile_function())
 
 
 def test_higher_order_daggerable_callable(validate):
