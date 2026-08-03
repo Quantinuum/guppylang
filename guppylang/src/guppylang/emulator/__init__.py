4dfe90292c7221b98b4756e59e146ac7a4b223bb
Agustín Borgna <agustin.borgna@quantinuum.com>
2026-08-03 13:47:39 +0100

    last dummy

--- guppylang/src/guppylang/emulator/__init__.py
+++ guppylang/src/guppylang/emulator/__init__.py
@@ -194,6 +194,7 @@ mappings to run the program with for each shot:
     ])
 
 .. code-block:: python
+
     # output
     EmulatorResult(results=[
         QsysShot(entries=[('doubled', 3.0), ('k1', 4)]),
@@ -228,6 +229,7 @@ code with the same compilation target, make sure the `platform` argument matches
 standard library used:
 
 .. code-block:: python
+
     from guppylang.std.qsystem.sol import phased_xx
     from guppylang.std.angles import pi
 
