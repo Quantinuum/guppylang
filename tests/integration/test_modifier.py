f619ccdfbbfdbe296be50d26a1d6bd7530e526d4
Nicola Assolini <nicola.assolini@quantinuum.com>
2026-08-06 16:00:23 +0200

    feat: make `__truediv__` daggerable (#2177)

--- tests/integration/test_modifier.py
+++ tests/integration/test_modifier.py
@@ -1,4 +1,5 @@
 import base64
+from guppylang.std.angles import angle
 import pytest
 
 from guppylang.decorator import guppy
@@ -17,7 +18,7 @@ from guppylang.std.builtins import (
     power,
 )
 from guppylang.std.num import nat
-from guppylang.std.quantum import cx, discard, discard_array, h, qubit
+from guppylang.std.quantum import cx, discard, discard_array, h, qubit, rx
 
 
 def test_dagger_simple(validate):
@@ -606,6 +607,12 @@ def test_std(validate):
         b = o.is_some()
         return 1 + 2.0 - 3 * 4 // y, o
 
+    @guppy(unitary=True)
+    def f(controller: qubit, target: qubit) -> None:
+        a = angle(1 / 3)
+        with control(controller):
+            rx(target, a)
+
     @guppy.comptime(daggerable=True)
     def test_arr() -> array[int, n]:
         x = array(y // y for i in range(n)).copy()
@@ -617,5 +624,10 @@ def test_std(validate):
         r = ok[int, qubit](42)
         _, oq = test(r)
         oq.unwrap_nothing()
+        c = qubit()
+        t = qubit()
+        f(c, t)
+        discard(c)
+        discard(t)
 
     validate(main.compile())
