d762133b235463516a6d9f10c51590a3bc8bcc38
Maximilian Rüsch <78490564+maximilianruesch@users.noreply.github.com>
2026-08-18 10:03:51 +0000

    test: Pull out error test case collection to common utility (#2195)

--- tests/error/test_nested_errors.py
+++ tests/error/test_nested_errors.py
@@ -1,21 +1,10 @@
-import pathlib
 import pytest
 
-from tests.error.util import run_error_test
+from tests.error.util import run_error_test, collect_error_test_cases
 from tests.conftest import experimental_features_enabled
 
-path = pathlib.Path(__file__).parent.resolve() / "nested_errors"
-files = [
-    x
-    for x in path.iterdir()
-    if x.is_file() and x.suffix == ".py" and x.name != "__init__.py"
-]
 
-# Turn paths into strings, otherwise pytest doesn't display the names
-files = [str(f) for f in files]
-
-
-@pytest.mark.parametrize("file", files)
+@pytest.mark.parametrize("file", collect_error_test_cases("nested_errors"))
 def test_nested_errors(file, capsys, snapshot):
     with experimental_features_enabled():
         run_error_test(file, capsys, snapshot)
--- tests/error/test_output_errors.py
+++ tests/error/test_output_errors.py
@@ -1,21 +1,13 @@
-import pathlib
 import pytest
 
-from tests.error.util import run_error_test
+from tests.error.util import run_error_test, collect_error_test_cases
 
-path = pathlib.Path(__file__).parent.resolve() / "output_errors"
-files = [
-    x
-    for x in path.iterdir()
-    if x.is_file() and x.suffix == ".py" and x.name != "__init__.py"
-]
 
-# TODO: Skip functional tests for now
-files = [f for f in files if "functional" not in f.name]
-
-# Turn paths into strings, otherwise pytest doesn't display the names
-files = [str(f) for f in files]
-
+files = collect_error_test_cases(
+    "output_errors",
+    # TODO: Skip functional tests for now
+    exclude=lambda path: path.is_file() and "functional" in path.name,
+)
 
 @pytest.mark.parametrize("file", files)
 def test_misc_errors(file, capsys, snapshot):
