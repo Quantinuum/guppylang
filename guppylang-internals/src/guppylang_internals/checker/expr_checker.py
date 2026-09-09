cce6be74b80e32b9d7da6066400c803f6ff9dc8b
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2026-09-09 08:48:47 +0100

    add are_capturing_closures_enabled, use it

--- guppylang-internals/src/guppylang_internals/checker/expr_checker.py
+++ guppylang-internals/src/guppylang_internals/checker/expr_checker.py
@@ -107,6 +107,7 @@ from guppylang_internals.error import (
     saved_exception_hook,
 )
 from guppylang_internals.experimental import (
+    are_capturing_closures_enabled,
     check_function_tensors_enabled,
     check_lists_enabled,
 )
@@ -1420,17 +1421,10 @@ def function_def_value_to_function_value(
     uniquely identifies them. Nested functions are materialised as local values, so
     their expression must be preserved to retain a possible closure.
     """
-    if isinstance(ty, NestedFunctionDefType):
-        from guppylang_internals.experimental import check_capturing_closures_enabled
-
-        try:
-            check_capturing_closures_enabled(expr)
-            # We don't record whether individual nested functions capture anything,
-            # so conservatively assume it might.
-            return with_type(ty.sig, expr)
-        except GuppyError:
-            # Capturing closures disabled
-            pass
+    if isinstance(ty, NestedFunctionDefType) and are_capturing_closures_enabled():
+        # We don't record whether individual nested functions capture anything,
+        # so conservatively assume it might.
+        return with_type(ty.sig, expr)
     name = ENGINE.get_parsed(ty.def_id).name
     return with_type(ty.sig, with_loc(expr, make_global_name(name, ty.def_id)))
 
